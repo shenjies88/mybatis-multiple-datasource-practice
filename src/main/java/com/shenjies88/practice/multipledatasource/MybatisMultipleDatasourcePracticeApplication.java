bd1d6ed680146ae1e2242d975f4d1b539e4e938f
shenjies88 <1961486254@qq.com>
2021-01-24 21:47:52 +0800

    init

---
+++ src/main/java/com/shenjies88/practice/multipledatasource/MybatisMultipleDatasourcePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.shenjies88.practice.multipledatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisMultipleDatasourcePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisMultipleDatasourcePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shenjies88/practice/multipledatasource/MybatisMultipleDatasourcePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shenjies88.practice.multipledatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisMultipleDatasourcePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
