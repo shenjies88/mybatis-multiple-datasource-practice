de4b8814fb8d7e99c29125ec0c27d273deefa7ca
shenjies88 <1961486254@qq.com>
2021-01-25 09:11:07 +0800

    fix:手动配置式实现读写分离

--- src/main/java/com/shenjies88/practice/multipledatasource/config/DataSourceConfig.java
+++ src/main/java/com/shenjies88/practice/multipledatasource/config/DataSourceConfig.java
@@ -2,14 +2,17 @@ package com.shenjies88.practice.multipledatasource.config;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.shardingsphere.api.config.masterslave.MasterSlaveRuleConfiguration;
+import org.apache.shardingsphere.shardingjdbc.api.MasterSlaveDataSourceFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.SQLException;
+import java.util.*;
 
 /**
  * @author shenjies88
@@ -23,16 +26,29 @@ public class DataSourceConfig {
     private final ShardingSphereConfig shardingSphereConfig;
 
     @Bean
-    DataSource dsOne() {
+    DataSource dsOne() throws SQLException {
         // Configure actual data sources
         Map<String, DataSource> dataSourceMap = new HashMap<>();
+        String masterName = "";
+        List<String> slaveName = new ArrayList<>();
         for (ShardingSphereConfig.DataSourceProperties dataSourceProperties : shardingSphereConfig.getDatasource()) {
+            BasicDataSource masterDataSource = new BasicDataSource();
+            masterDataSource.setUrl(dataSourceProperties.getUrl());
+            masterDataSource.setUsername(dataSourceProperties.getUsername());
+            masterDataSource.setPassword(dataSourceProperties.getPassword());
+            dataSourceMap.put(dataSourceProperties.getName(), masterDataSource);
+            if (dataSourceProperties.getMaster()) {
+                masterName = dataSourceProperties.getName();
+            } else {
+                slaveName.add(dataSourceProperties.getName());
+            }
         }
-        return null;
+        MasterSlaveRuleConfiguration masterSlaveRuleConfig = new MasterSlaveRuleConfiguration("ds_master_slave", masterName, slaveName);
+        return MasterSlaveDataSourceFactory.createDataSource(dataSourceMap, masterSlaveRuleConfig, new Properties());
     }
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.dynamic.datasource.datasource2")
+    @ConfigurationProperties(prefix = "spring.dynamic.datasource.datasource2")
     DataSource dsTwo() {
         return DataSourceBuilder.create().build();
     }
